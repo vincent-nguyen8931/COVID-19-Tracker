d08ca55b8863e99fc7b6c36b260393f912f2c3e3
Austin W <austinwoo123@gmail.com>
2021-01-08 22:26:40 -0800

    display the searched state onto the page

--- assets/script.js
+++ assets/script.js
@@ -1,8 +1,6 @@
 $(document).ready(function () {
 
-  var newDiv = $("<div>")
-  console.log(newDiv)
-  var covidInfoBox = $("#covid-info").append(newDiv)
+
 
   // geolocte api 
   //  grab location
@@ -10,35 +8,35 @@ $(document).ready(function () {
   // testing locations
   // choosing testing locatioin
   // be provided directions for testing location
-  QueryURL = "https://discover.search.hereapi.com/v1/discover?apikey=cm9nka7Eq7NC7YfrsKwehxVumUyYYWiARjJBuXRa484&q=Covid&at=30.22,-92.02&limit=3"
+  // QueryURL = "https://discover.search.hereapi.com/v1/discover?apikey=cm9nka7Eq7NC7YfrsKwehxVumUyYYWiARjJBuXRa484&q=Covid&at=30.22,-92.02&limit=3"
 
-  $.ajax({
-    url: QueryURL,
-    method: "GET"
-  }).then(function (currentDay) {
-    console.log(currentDay);
-  });
+  // $.ajax({
+  //   url: QueryURL,
+  //   method: "GET"
+  // }).then(function (currentDay) {
+  //   console.log(currentDay);
+  // });
 
-  // on click
-  // this.val(items[i])
-
-
-  // QueryURL2 = "https://cors-anywhere.herokuapp.com/https://maps.googleapis.com/maps/api/place/findplacefromtext/jsonp?origin=Lafayette&units=imperial&key=AIzaSyDs01d715oubUTbz2ZrZSYWVH-k7N9n9xI"
+  // // on click
+  // // this.val(items[i])
 
 
-  // places google
-  // QueryURL2 = "https://cors-anywhere.herokuapp.com/https://maps.googleapis.com/maps/api/place/nearbysearch/json?location=-33.8670522,151.1957362&radius=1500&type=restaurant&keyword=cruise&key=AIzaSyDs01d715oubUTbz2ZrZSYWVH-k7N9n9xI"
+  // // QueryURL2 = "https://cors-anywhere.herokuapp.com/https://maps.googleapis.com/maps/api/place/findplacefromtext/jsonp?origin=Lafayette&units=imperial&key=AIzaSyDs01d715oubUTbz2ZrZSYWVH-k7N9n9xI"
 
-  // covid data by country/state
-  QueryURL2 = "https://coronavirus-smartable.p.rapidapi.com/stats/v1/US/?rapidapi-key=60cc0bce2emsh9ba3c88eb3c4d5dp125545jsnc79365a8f484"
 
-  $.ajax({
-    url: QueryURL2,
-    method: "GET"
-  }).then(function (currentDay) {
+  // // places google
+  // // QueryURL2 = "https://cors-anywhere.herokuapp.com/https://maps.googleapis.com/maps/api/place/nearbysearch/json?location=-33.8670522,151.1957362&radius=1500&type=restaurant&keyword=cruise&key=AIzaSyDs01d715oubUTbz2ZrZSYWVH-k7N9n9xI"
 
-    console.log(currentDay);
-  });
+  // // covid data by country/state
+  // QueryURL2 = "https://coronavirus-smartable.p.rapidapi.com/stats/v1/US/?rapidapi-key=60cc0bce2emsh9ba3c88eb3c4d5dp125545jsnc79365a8f484"
+
+  // $.ajax({
+  //   url: QueryURL2,
+  //   method: "GET"
+  // }).then(function (currentDay) {
+
+  //   console.log(currentDay);
+  // });
   let map;
 
   function initMap() {
@@ -51,11 +49,16 @@ $(document).ready(function () {
   initMap();
 
 
-  var searchTerm = $("#search-term").val()
 
-  $("#search-button").click(function () {
+  var provinceOrState = $("<div>")
+  var totalConfirmedCases = $("<div>")
+  var totalDeaths = $("<div>")
+  var totalRecoveredCases = $("<div>")
+
+  $("#search-button").click(function (event) {
+    event.preventDefault()
     Queryurl3 = "https://coronavirus-smartable.p.rapidapi.com/stats/v1/US/?rapidapi-key=60cc0bce2emsh9ba3c88eb3c4d5dp125545jsnc79365a8f484"
-    console.log(Queryurl3);
+    // console.log(Queryurl3);
 
     // var cityName;
     // var searchTerm = $("#search-term").val()
@@ -66,63 +69,33 @@ $(document).ready(function () {
     }).then(function (covidInfo) {
       console.log(covidInfo);
 
-
       // *needs fixing* //
       // button here
       var covidInfoBox = $("#covid-info")
-      // $("#search-button").click(function () {
+      $("#search-button").click(function () {
 
-      // console.log("hello world");
+        var searchTerm = $("#findState").val()
+        console.log(typeof searchTerm)
 
-      // var searchTerm = $("#search-term").val()
-      // console.log(searchTerm)
-      // for (i = 0; i < covidInfo.stats.breakdowns.length; i++);
-      console.log(searchTerm);
-      console.log(covidInfo.stats.breakdowns[0].location.provinceOrState);
-      // if (searchTerm == covidInfo.stats.breakdowns[].location.provinceOrState);
+        var arrayLength = covidInfo.stats.breakdowns.length
 
-      //   var provinceOrState = $("<div>").text(covidInfo.stats.breakdowns[i].location.provinceOrState);
-      //   var totalConfirmedCases = $("<div>").text(covidInfo.stats.breakdowns[i].totalConfirmedCases);
-      //   var totalDeaths = $("<div>").text(covidInfo.stats.breakdowns[i].totalDeaths);
-      //   var totalRecoveredCases = $("<div>").text(covidInfo.stats.breakdowns[i].totalRecoveredCases);
+        for (i = 0; i < 62; i++) {
+          console.log(searchTerm);
+          // console.log(covidInfo.stats.breakdowns[0].location.provinceOrState);
+          var state = covidInfo.stats.breakdowns[i].location.provinceOrState
+          if (searchTerm === state) {
 
-      //   covidInfoBox.append(provinceOrState, totalConfirmedCases, totalDeaths, totalRecoveredCases);
+            provinceOrState.text(covidInfo.stats.breakdowns[i].location.provinceOrState);
+            totalConfirmedCases.text(covidInfo.stats.breakdowns[i].totalConfirmedCases);
+            totalDeaths.text(covidInfo.stats.breakdowns[i].totalDeaths);
+            totalRecoveredCases.text(covidInfo.stats.breakdowns[i].totalRecoveredCases);
+
+            covidInfoBox.append(provinceOrState, totalConfirmedCases, totalDeaths, totalRecoveredCases);
+
+          }
+        }
+
+      })
     })
-
-
-
-
-
-  });
-
-  // // button here
-  // var covidInfoBox = $("#covid-info")
-  // $("#search-button").click(function (event) {
-  //   console.log(event);
-  //   // console.log("hello world");
-
-  //   var searchTerm = $("#search-term").val()
-  //   console.log(searchTerm)
-  //   for (i = 0; i < stats.breakdowns.length; i++);
-  //   if (searchTerm === stats.breakdowns[i].location.provinceOrState);
-
-  //   var provinceOrState = $("<div>").text(stats.breakdowns[i].location.provinceOrState);
-  //   var totalConfirmedCases = $("<div>").text(stats.breakdowns[i].totalConfirmedCases);
-  //   var totalDeaths = $("<div>").text(stats.breakdowns[i].totalDeaths);
-  //   var totalRecoveredCases = $("<div>").text(stats.breakdowns[i].totalRecoveredCases);
-
-  //   covidInfoBox.append(provinceOrState, totalConfirmedCases, totalDeaths, totalRecoveredCases);
-  // })
-
-  // for (j = 0 j < length; i++)
-
-
+  })
 })
-
-
-
-
-
-
-
-
