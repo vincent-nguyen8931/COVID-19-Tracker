c92817ab0ce1cc59706d80bb042a9ee70bdf3eed
Austin W <austinwoo123@gmail.com>
2021-01-08 17:48:33 -0800

    add info to city box code

--- assets/script.js
+++ assets/script.js
@@ -1,6 +1,8 @@
 $(document).ready(function () {
 
-
+  var newDiv = $("<div>")
+  console.log(newDiv)
+  var covidInfoBox = $("#covid-info").append(newDiv)
 
   // geolocte api 
   //  grab location
@@ -48,33 +50,70 @@ $(document).ready(function () {
 
   initMap();
 
-  Queryurl3 = "https://coronavirus-smartable.p.rapidapi.com/stats/v1/US/?rapidapi-key=60cc0bce2emsh9ba3c88eb3c4d5dp125545jsnc79365a8f484"
 
-  $.ajax({
-    url: Queryurl3,
-    method: "GET"
-  }).then(function (currentDay) {
-    console.log(currentDay);
+  var searchTerm = $("#search-term").val()
+
+  $("#search-button").click(function () {
+    Queryurl3 = "https://coronavirus-smartable.p.rapidapi.com/stats/v1/US/?rapidapi-key=60cc0bce2emsh9ba3c88eb3c4d5dp125545jsnc79365a8f484"
+    console.log(Queryurl3);
+
+    // var cityName;
+    // var searchTerm = $("#search-term").val()
+    // cityName = searchTerm
+    $.ajax({
+      url: Queryurl3,
+      method: "GET"
+    }).then(function (covidInfo) {
+      console.log(covidInfo);
+
+
+      // *needs fixing* //
+      // button here
+      var covidInfoBox = $("#covid-info")
+      // $("#search-button").click(function () {
+
+      // console.log("hello world");
+
+      // var searchTerm = $("#search-term").val()
+      // console.log(searchTerm)
+      // for (i = 0; i < covidInfo.stats.breakdowns.length; i++);
+      console.log(searchTerm);
+      console.log(covidInfo.stats.breakdowns[0].location.provinceOrState);
+      // if (searchTerm == covidInfo.stats.breakdowns[].location.provinceOrState);
+
+      //   var provinceOrState = $("<div>").text(covidInfo.stats.breakdowns[i].location.provinceOrState);
+      //   var totalConfirmedCases = $("<div>").text(covidInfo.stats.breakdowns[i].totalConfirmedCases);
+      //   var totalDeaths = $("<div>").text(covidInfo.stats.breakdowns[i].totalDeaths);
+      //   var totalRecoveredCases = $("<div>").text(covidInfo.stats.breakdowns[i].totalRecoveredCases);
+
+      //   covidInfoBox.append(provinceOrState, totalConfirmedCases, totalDeaths, totalRecoveredCases);
+    })
+
+
+
+
+
   });
 
-  // button here
-  var covidInfoBox = $("#covid-info")
-  $("#search-button").on("click", "button", (function (event) {
-    console.log(event)
+  // // button here
+  // var covidInfoBox = $("#covid-info")
+  // $("#search-button").click(function (event) {
+  //   console.log(event);
+  //   // console.log("hello world");
 
-    var searchTerm = $("#search-term").val()
-    console.log(searchTerm)
-    for (i = 0; i < breakdowns.length; i++);
-    if (searchTerm === stats.breakdowns[i].location.provinceOrState);
+  //   var searchTerm = $("#search-term").val()
+  //   console.log(searchTerm)
+  //   for (i = 0; i < stats.breakdowns.length; i++);
+  //   if (searchTerm === stats.breakdowns[i].location.provinceOrState);
 
-    var provinceOrState = $("<div>").text(stats.breakdowns[i].location.provinceOrState);
-    var totalConfirmedCases = $("<div>").text(stats.breakdowns[i].totalConfirmedCases);
-    var totalDeaths = $("<div>").text(stats.breakdowns[i].totalDeaths);
-    var totalRecoveredCases = $("<div>").text(stats.breakdowns[i].totalRecoveredCases);
+  //   var provinceOrState = $("<div>").text(stats.breakdowns[i].location.provinceOrState);
+  //   var totalConfirmedCases = $("<div>").text(stats.breakdowns[i].totalConfirmedCases);
+  //   var totalDeaths = $("<div>").text(stats.breakdowns[i].totalDeaths);
+  //   var totalRecoveredCases = $("<div>").text(stats.breakdowns[i].totalRecoveredCases);
 
-    covidInfoBox.append(provinceOrState, totalConfirmedCases, totalDeaths, totalRecoveredCases);
-  })
-  )
+  //   covidInfoBox.append(provinceOrState, totalConfirmedCases, totalDeaths, totalRecoveredCases);
+  // })
+
   // for (j = 0 j < length; i++)
 
 
@@ -87,4 +126,3 @@ $(document).ready(function () {
 
 
 
-  // $("<div>").text(stats.)
