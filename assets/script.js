50ebd18adab716182206bea90d134b8b9c8ec8aa
Austin W <austinwoo123@gmail.com>
2021-01-10 16:32:42 -0800

    change color for pie chart

--- assets/script.js
+++ assets/script.js
@@ -65,8 +65,8 @@ $(document).ready(function () {
               labels: ['Total Confirmed Cases', 'Total Recovered Cases', 'Total Deaths'],
               datasets: [{
                 label: 'My First dataset',
-                backgroundColor: ['rgb(255, 99, 132)', 'rgb(230,80,130)', 'rgb(200,50,120)'],
-                borderColor: 'rgb(255, 99, 132)',
+                backgroundColor: ['rgba(0, 0, 255, 0.6)', 'rgba(255, 255, 0, 0.9)', 'red'],
+                borderColor: 'rgba(0, 209, 178)',
                 data: [covidInfo.stats.breakdowns[i].totalConfirmedCases,
                 covidInfo.stats.breakdowns[i].totalRecoveredCases,
                 covidInfo.stats.breakdowns[i].totalDeaths],
